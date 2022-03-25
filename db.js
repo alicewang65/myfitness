5bc9285cf15bf96e2094cd3a41ef84e04d75e2bd
Alice Wang <alice.wang428@gmail.com>
2022-03-25 12:00:16 -0400

    commented schema

--- db.js
+++ db.js
@@ -2,18 +2,25 @@ const mongoose = require('mongoose'),
 	URLSlugs = require('mongoose-url-slugs'),
 	passportLocalMongoose = require('passport-local-mongoose');
 
+// User Schema, stores their username, password, and a
+// reference to their (workout) log
 const User = new mongoose.Schema({
 	username: {type: String, required: true},
 	password: {type: String, required: true},
   	log: { type: mongoose.Schema.Types.ObjectId, ref: 'Log' }
 });
 
+// Entry Schema, represents an entry, with fields for
+// title, date and description (main part of the entry)
 const Entry = new mongoose.Schema({
 	title: {type: String, required: true},
 	date: {type: String, required: false},
 	description: {type: String, required: true}
 });
 
+// Log Schema, represents a user's log, contains embedded
+// entries, since user's will be able to see their
+// entries all at once
 const Log = new mongoose.Schema({
 	user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
 	items: [Entry]
