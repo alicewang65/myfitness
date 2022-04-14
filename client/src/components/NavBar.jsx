c851042f8314ed9a41c82a109c46974f91801893
Alice Wang <alice.wang428@gmail.com>
2022-04-13 20:12:25 -0400

    createRoot instead of ReactDOM

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,19 +4,19 @@ import {Link} from "react-router-dom"
 
 export function NavBar() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <Link class="navbar-brand" to="/home">MyFitness</Link>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/home">MyFitness</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/register">Register</Link>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/register">Register</Link>
                         </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="/login">Login</Link>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
                         </li>
                     </ul>
                 </div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Register } from './components/Register';
 import { Login } from "./components/Login";
-import { User } from "./components/User";
 import { AddEntry } from './components/AddEntry';
 import {AllEntries} from "./components/AllEntries";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-import { NavBar } from './components/NavBar';
 import { Home } from './components/Home';
 import { HomeLoggedIn } from './components/HomeLoggedIn';
 import { NotFound } from './components/NotFound';
 
-ReactDOM.render(
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
@@ -26,9 +27,9 @@ ReactDOM.render(
         {/* <Route path="/user" element={<User />}/> */}
         <Route exact path="/create/:id" element={<AddEntry/>}/>
         <Route exact path="/entries/:id" element={<AllEntries />}/>
+        <Route exact path="/" element={<Navigate replace to="/home" />}/>
         <Route path="*" element={<NotFound />}/>
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
