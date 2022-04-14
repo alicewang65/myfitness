067ad27ef53bb3a540e6fd2f58ce2f3df1627094
Alice Wang <alice.wang428@gmail.com>
2022-04-13 20:03:05 -0400

    restructured and cleaned repo

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use(express.static(path.join(__dirname, "client", "build")))
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors({
-    // origin: "http://localhost:8000",
-    origin: "https://alice-wang-ait-final-project.herokuapp.com/",
+    origin: "http://localhost:8000",
+    // origin: "https://alice-wang-ait-final-project.herokuapp.com/",
     credentials: true
 }));
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Register } from './components/Register';
 import { Login } from "./components/Login";
 import { User } from "./components/User";
-import reportWebVitals from './reportWebVitals';
 import { AddEntry } from './components/AddEntry';
 import {AllEntries} from "./components/AllEntries";
 
@@ -33,8 +32,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
