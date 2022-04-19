aa935737e3174d87545dbf426165929937a5b04f
Alice Wang <alice.wang428@gmail.com>
2022-04-19 13:04:11 -0400

    implemented log out feature

--- client/src/components/NavBarLoggedIn.jsx
+++ client/src/components/NavBarLoggedIn.jsx
@@ -1,25 +1,36 @@
 import React from "react"
-import {Link, useParams} from "react-router-dom"
+import { Link, Navigate, useNavigate } from "react-router-dom"
+import api from "../api.js";
 
 
 export function NavBarLoggedIn() {
-    // need to get ID of user once logged in
-    const params = useParams();
+    const navigate = useNavigate();
+
+    const logOut = async () => {
+        const res = await api.logOutUser();
+        if (Object.hasOwnProperty.call(res.data, "success")) {
+            navigate("/home");
+        }
+    };
 
     return (
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-                <Link class="navbar-brand" to={"/home/" + params.username}>MyFitness</Link>
+                <Link class="navbar-brand" to={"/home"}>MyFitness</Link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <Link class="nav-link" to={"/entries/" + params.username}>Entries</Link>
+                            <Link class="nav-link" to={"/entries"}>Entries</Link>
                         </li>
                         <li class="nav-item">
-                            <Link class="nav-link" to={"/create/" + params.username}>Add Entry</Link>
+                            <Link class="nav-link" to={"/create"}>Add Entry</Link>
+                        </li>
+                        <li class="nav-item">
+                            <button class="nav-link no-button" onClick={logOut}>Log Out</button>
+                            {/* <Link class="nav-link" onClick={logOut}>Log Out</Link> */}
                         </li>
                     </ul>
                 </div>
