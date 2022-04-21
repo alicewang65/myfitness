39df5c6efea44b562554eaeb68f61f5ef04c77d5
Alice Wang <alice.wang428@gmail.com>
2022-04-21 18:28:26 -0400

    styled add an entry page

--- client/src/components/AddEntry.jsx
+++ client/src/components/AddEntry.jsx
@@ -48,37 +48,45 @@ export function AddEntry() {
     };
 
     return (
-        <div>
+        <div className="m-4">
             <NavBarLoggedIn />
-            <h1>Create a New Entry</h1>
-            <form onSubmit={submit}>
-                <section>
-                    <label for="title">Title</label>
-                    <input id="title" 
-                        name="title" 
-                        type="text" 
-                        onChange={titleHandler}
-                        required />
-                </section>
-                <section>
-                    <label for="date">Date</label>
-                    <input id="date" 
-                        name="date" 
-                        type="date" 
-                        onChange={dateHandler}
-                        required />
-                </section>
-                <section>
-                    <label for="entry">Entry</label>
-                    <textarea id="entry"
-                        name="entry"
-                        onChange={entryHandler}
-                        rows="5"
-                        placeholder="Enter your training entry here..."
-                        required/>
-                </section>
-                <button type="submit">Add Entry</button>
-            </form>
+            <h1 className="my-3 text-center">Create a New Entry</h1>
+
+            <div className="w-75 mx-auto">
+                <form onSubmit={submit}>
+                    <div className="form-group mt-3">
+                        <label htmlFor="title">Title</label>
+                        <input className="form-control"
+                            id="title" 
+                            name="title" 
+                            type="text" 
+                            onChange={titleHandler}
+                            required />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label htmlFor="date">Date</label>
+                        <input className="form-control"
+                            id="date" 
+                            name="date" 
+                            type="date" 
+                            onChange={dateHandler}
+                            required />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label htmlFor="entry">Entry</label>
+                        <textarea className="form-control"
+                            id="entry"
+                            name="entry"
+                            onChange={entryHandler}
+                            rows="5"
+                            placeholder="Enter your training entry here..."
+                            required/>
+                    </div>
+                    <div className="text-center mt-3">
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
